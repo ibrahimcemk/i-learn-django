f93f7e6c8a7e588745861c568c9c1cd2f9483950
İbrahim Cem Keleş <ibrahimmcemkeles@gmail.com>
2026-08-19 17:54:48 +0300

    corporate dosyalarını ana dizine oluşturdum.

--- corporate/corporate/settings.py
+++ corporate/corporate/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)9t)=tf8ftpkk116h9^(^$fx*30=1yg(=nrux(&*zjgj2e-z7&'
+SECRET_KEY = 'django-insecure-01i3_n_f1uli6xeto1z51gz)uh@@6byr&@e0snt#g7&zqa@y#0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
